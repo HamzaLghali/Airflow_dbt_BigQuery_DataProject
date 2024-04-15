7e1b94ae51b47de4821f75e0c26ed94cdfa0ae6f
HamzaLghali <92524604+HamzaLghali@users.noreply.github.com>
2024-04-15 12:16:44 +0100

    2com

--- dags/retail.py
+++ dags/retail.py
@@ -6,6 +6,11 @@ from astro import sql as aql
 from astro.files import File
 from astro.sql.table import Table, Metadata
 from astro.constants import FileType
+from include.dbt.cosmos_config import DBT_PROJECT_CONFIG, DBT_CONFIG
+from cosmos.airflow.task_group import DbtTaskGroup
+from cosmos.constants import LoadMode
+from cosmos.config import ProjectConfig, RenderConfig
+
 
 @dag(
     start_date=datetime(2024,4,3),
@@ -56,4 +61,16 @@ def retail():
 
 
     check_load()
+
+
+    transform = DbtTaskGroup(
+        group_id='transform',
+        project_config=DBT_PROJECT_CONFIG,
+        profile_config=DBT_CONFIG,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_LS,
+            select=['path:models/transform']
+        )
+    )
+
 retail()
