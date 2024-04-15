98b7a621bd76c00abaf687566fae68dbe8c9c8d9
HamzaLghali <92524604+HamzaLghali@users.noreply.github.com>
2024-04-15 17:03:49 +0100

    commit last

--- dags/retail.py
+++ dags/retail.py
@@ -90,4 +90,24 @@ def retail():
         from include.soda.check_function import check
 
         return check(scan_name, checks_subpath)
+
+
+
+    report = DbtTaskGroup(
+        group_id='report',
+        project_config=DBT_PROJECT_CONFIG,
+        profile_config=DBT_CONFIG,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_LS,
+            select=['path:models/report']
+        )
+    )    
+
+
+
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_report(scan_name='check_report', checks_subpath='report'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
 retail()
