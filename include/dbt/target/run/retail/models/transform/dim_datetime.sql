49c433ac3872d1b25f736baffcdc5ab2c9b6484f
HamzaLghali <92524604+HamzaLghali@users.noreply.github.com>
2024-04-15 12:02:51 +0100

    first commit

---
+++ include/dbt/target/run/retail/models/transform/dim_datetime.sql
@@ -0,0 +1,31 @@
+
+  
+    
+
+    create or replace table `airflow-dbt-bigquery`.`retail`.`dim_datetime`
+    
+    
+
+    OPTIONS()
+    as (
+      -- Create a CTE to extract date and time components
+WITH datetime_cte AS (  
+  SELECT DISTINCT
+    InvoiceDate AS datetime_id,
+    -- Since the dates are in the format "MM/DD/YYYY HH:MM", we use this format
+    PARSE_DATETIME('%m/%d/%Y %H:%M', InvoiceDate) AS date_part
+  FROM `airflow-dbt-bigquery`.`retail`.`raw_invoices`
+  WHERE InvoiceDate IS NOT NULL
+)
+SELECT
+  datetime_id,
+  date_part as datetime,
+  EXTRACT(YEAR FROM date_part) AS year,
+  EXTRACT(MONTH FROM date_part) AS month,
+  EXTRACT(DAY FROM date_part) AS day,
+  EXTRACT(HOUR FROM date_part) AS hour,
+  EXTRACT(MINUTE FROM date_part) AS minute,
+  EXTRACT(DAYOFWEEK FROM date_part) AS weekday
+FROM datetime_cte
+    );
+  
